1b613d98ee38e5d8d924760fc8a5a3da572a7e38
bangarpetpanipuri2026-ops <bangarpetpanipuri2026@gmail.com>
2026-08-19 13:15:43 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAnRBYNUC1Z7CeuYbV7zyMDsVOme0LCPqk",
+  authDomain: "chaya-co.firebaseapp.com",
+  databaseURL: "https://chaya-co-default-rtdb.firebaseio.com",
+  projectId: "chaya-co",
+  storageBucket: "chaya-co.firebasestorage.app",
+  messagingSenderId: "378388457746",
+  appId: "1:378388457746:web:520850fb34170eba0eebcb"
+};
+
+firebase.initializeApp(firebaseConfig);
